acca7951501d32f380eb2979dffa47b559505745
Fagner Fontes Batista <fagnerfontesbatista@hotmail.com>
2022-07-19 22:16:19 -0300

    Atualização de proejeto LabCommerce

---
+++ src/FormDireito.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './FormDireito.css'
+function FormDireito (){
+return (
+    <div className="corpo">
+    <h1 className="tituloDireito">Carrinho</h1>
+    <button>Remover</button>
+
+    </div>
+)
+}
+
+export default FormDireito
